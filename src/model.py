8fe058ac3751130612aea632203c4715e2512e22
SalwaMustafa <salwa2532003@gmail.com>
2025-02-11 14:07:59 +0200

    Add a prompt

--- src/model.py
+++ src/model.py
@@ -5,10 +5,12 @@ checkpoint = "Salwaaaa/t5-base-finetuned-summarize-dailymail"
 tokenizer = AutoTokenizer.from_pretrained(checkpoint)
 model = AutoModelForSeq2SeqLM.from_pretrained(checkpoint)
 
+prompt = "You are the smartest model ever. Please summarize the following text in a concise manner:\n\n"
+
 def generate_summary(text):
 
-    inputs = tokenizer(text, return_tensors = "pt")
-    summary_ids = model.generate(inputs['input_ids'], max_length = 75 )
+    inputs = tokenizer((prompt + text), max_length=512, truncation=True, return_tensors="pt")
+    summary_ids = model.generate(inputs['input_ids'], max_length = 100 )
     summary = tokenizer.decode(summary_ids[0], skip_special_tokens = True)
 
     return summary
